920a97e11a3f9888004ea68983233d0ce50e1ea3
Liam Hetherington <liambhetherington@gmail.com>
2019-05-07 21:04:03 +0000

    created a function to list table of urls and shortened value

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
+
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +22,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
