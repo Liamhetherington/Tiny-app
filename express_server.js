f6c84c2e7e48518fd07eaeccddf248257ac7920a
Liam Hetherington <liambhetherington@gmail.com>
2019-05-07 23:42:08 +0000

    added function that will generate a random short URL from input for url

--- express_server.js
+++ express_server.js
@@ -4,8 +4,12 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
 
+}
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,6 +35,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
