6ef32f3b504f2b9ae9c254d76efd4b9273370f04
Liam Hetherington <liambhetherington@gmail.com>
2019-05-07 21:43:55 +0000

    added a second route and template

--- express_server.js
+++ express_server.js
@@ -29,6 +29,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let shortenedurl = req.params.shortURL;
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortenedurl] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
