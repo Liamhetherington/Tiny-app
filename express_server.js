7d54fb7ac2c5e65c87353357a6c093f83d28adda
Liam Hetherington <liambhetherington@gmail.com>
2019-05-08 00:45:21 +0000

    redirects for and stores shortened URL to database after it is submitted

--- express_server.js
+++ express_server.js
@@ -1,15 +1,21 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const crypto = require("crypto");
+
 
 app.set("view engine", "ejs");
 
 function generateRandomString() {
-
+  return crypto.randomBytes(6).toString("hex").substring(0, 6);
 }
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+function addedtoDatabase (shortURL, longURL) {
+  urlDatabase[shortURL] = longURL;
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,6 +30,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -37,17 +44,22 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
 app.get("/urls/:shortURL", (req, res) => {
   let shortenedurl = req.params.shortURL;
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortenedurl] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  addedtoDatabase(shortURL, longURL);
+  console.log(urlDatabase);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+
 });
